78683e86d7d655ef97635fdfbea52a3b1cff9ef5
Martin Saldinger <martin.saldinger@gmail.com>
2024-11-17 04:41:08 +0700

    add timestamps

--- subgraph/src/RebalancingOperator.ts
+++ subgraph/src/RebalancingOperator.ts
@@ -4,13 +4,16 @@ import { Rebalance as RebalanceEntity } from "../generated/schema";
 
 export function handleRebalance(rebalanceEvent: RebalanceEvent): void {
 	let rebalanceEntity = new RebalanceEntity(
-		rebalanceEvent.transaction.hash.concatI32(rebalanceEvent.logIndex),
+		rebalanceEvent.transaction.hash.concatI32(rebalanceEvent.logIndex.toI32()),
 	);
 
 	rebalanceEntity.from = rebalanceEvent.params.from;
 	rebalanceEntity.to = rebalanceEvent.params.to;
 	rebalanceEntity.amount = rebalanceEvent.params.amount;
 	rebalanceEntity.onBehalfOfAccount = rebalanceEvent.params.onBehalfOfAccount;
+	rebalanceEntity.blockNumber = rebalanceEvent.block.number;
+	rebalanceEntity.blockTimestamp = rebalanceEvent.block.timestamp;
+	rebalanceEntity.transactionHash = rebalanceEvent.transaction.hash;
 
 	rebalanceEntity.save();
 
