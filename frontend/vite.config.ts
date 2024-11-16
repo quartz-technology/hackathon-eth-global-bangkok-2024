2e097da663f7a02a294299c9b5063df1c3183b21
Martin Saldinger <martin.saldinger@gmail.com>
2024-11-16 23:08:33 +0700

    remove base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/reactjs-template",
 	plugins: [
 		// Allows using React dev server along with building a React application with Vite.
 		// https://npmjs.com/package/@vitejs/plugin-react-swc
