5ec5db7a8579bd2fe33e373415e75e53138ed5d2
Isma <71719097+Doozers@users.noreply.github.com>
2024-11-17 07:38:29 +0700

    docs: update SC docs
    Signed-off-by: Isma <71719097+Doozers@users.noreply.github.com>

--- contracts/src/RebalancingOperator.sol
+++ contracts/src/RebalancingOperator.sol
@@ -8,9 +8,7 @@ import "solmate/tokens/ERC4626.sol";
 import "openzeppelin/access/Ownable.sol";
 
 /// @title RebalancingOperator
-/// @notice This contract allows anyone, in exchange for a tip, to pull liquidity out
-/// of a heavily utilised vault on behalf of someone else. Thanks to this operator,
-/// a user can delegate the monitoring of their vault to someone else and go on with their life.
+/// @notice This contract allows anyone, to set an operator that can rebalance the positions to track better yields.
 contract RebalancingOperator is Ownable {
     using SafeTransferLib for ERC20;
 
@@ -24,17 +22,15 @@ contract RebalancingOperator is Ownable {
         asset = _asset;
     }
 
-    /// @notice Allows anyone to withdraw on behalf of a onBehalfOfAccount.
-    /// @dev Assumes that the onBehalfOfAccount owner had authorized the operator to withdraw on their behalf.
-    /// @param from The address of the position to rebalance.
-    /// @param to The new target address.
+    /// @notice Allows operator to rebalance a position from one vault to another.
+    /// @param from The vault address of the position to rebalance.
+    /// @param to The new vault address.
     /// @param onBehalfOfAccount The address of the account on behalf of which the operation is being executed.
     function rebalanceOnBehalf(address from, address to, address onBehalfOfAccount) external onlyOwner {
         uint256 maxWithdraw = ERC4626(from).maxWithdraw(onBehalfOfAccount);
 
         if (maxWithdraw == 0) return;
 
-        // if there's anything to withdraw, withdraw it to this contract
         evc.call(
             from,
             onBehalfOfAccount,
